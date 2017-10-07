9435c667e6beacbfdd171499c906e619dc36e1f8
Kedwin Chen <kedwinchen@gmail.com>
2017-10-07 01:08:38 -0700

    Fix escaping

--- ScoreEngine/resources/scripts/img_setup.sh
+++ ScoreEngine/resources/scripts/img_setup.sh
@@ -11,7 +11,7 @@ fi
 ${PKGINSTALL} sox libsox-fmt-all notify-osd dos2unix curl git automake python-gtk2
 
 cd /tmp/
-git clone https://github.com/neurobin/shc.git
+git clone https://github.com/neurobin/shc.git ./shc
 cd ./shc
 ./configure
 make
@@ -26,7 +26,7 @@ bash ${SESCRIPTS}/setup_banner.sh
 
 echo <<- _EOF_ > /usr/local/bin/score
 #!/bin/bash
-if [[ $EUID -ne 0 ]]; then
+if [[ \$EUID -ne 0 ]]; then
    echo "You must be root to run this script!"
    exit 1
 fi
@@ -34,7 +34,7 @@ fi
 /bin/rm /opt/ScoreEngine/ScoringEngine
 /usr/bin/gcc /opt/ScoreEngine/ScoringEngine.sh.x.c -o /opt/ScoreEngine/ScoringEngine
 /opt/ScoreEngine/ScoringEngine 2> /dev/null
-if [[ $? -ne 0 ]]; then
+if [[ \$? -ne 0 ]]; then
        echo "Something went wrong..."
        exit 1
 else
@@ -49,30 +49,30 @@ echo "PLEASE NOTE THAT THIS SCORING ENGINE HAS NOT YET BEEN CONFIGURED TO USE sy
 cat << _EOF_ > /etc/rc6.d/K99initScoring
 #!/bin/bash
 
-starttime=$(cat /usr/local/starttime)
-elapsedtime=$(cat /usr/local/elapsedtime)
+starttime=\$(cat /usr/local/starttime)
+elapsedtime=\$(cat /usr/local/elapsedtime)
 
 
 sessiontime=0
 
-if [[ "$starttime" == "" ]]; then
+if [[ "\$starttime" == "" ]]; then
 	sessiontime=0
 else
-	time="$(date +%s)"
-	sessiontime=$((time-starttime))
+	time="\$(date +%s)"
+	sessiontime=\$((time-starttime))
 fi
 
-if [[ "$elapsedtime" == "" ]]; then
-	totaltime=$sessiontime
-	if [[ $totaltime == 0 ]]; then
+if [[ "\$elapsedtime" == "" ]]; then
+	totaltime=\$sessiontime
+	if [[ \$totaltime == 0 ]]; then
 	    echo > /usr/local/elapsedtime
 	else
-	    echo $totaltime > /usr/local/elapsedtime
+	    echo \$totaltime > /usr/local/elapsedtime
 	fi
 else
 
-	totaltime=$((elapsedtime+sessiontime))
-	echo $totaltime > /usr/local/elapsedtime
+	totaltime=\$((elapsedtime+sessiontime))
+	echo \$totaltime > /usr/local/elapsedtime
 
 fi
 
@@ -82,7 +82,7 @@ _EOF_
 touch /usr/local/starttime
 touch /usr/local/elapsedtime
 
-readonly ACCOUNT=$(grep -i -- 'ACCOUNT=' ScoringEngine.sh |cut -d '=' -f2)
+readonly ACCOUNT=$(grep -i -- 'ACCOUNT=' ${SEDIRECTORY}/ScoringEngine.sh |cut -d '=' -f2)
 for DESKTOP_FILE in ReadMe ScoreReport UpdateScore ; do
   cp ${SESCRIPTS}/${DESKTOP_FILE}.desktop /home/${ACCOUNT}/Desktop/
 done
